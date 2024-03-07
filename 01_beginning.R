f2d15a77ced13f3ee85c52c5eb92cc8ad40bfc86
dariopratelli <98481720+dariopratelli@users.noreply.github.com>
2024-03-07 17:05:04 +0100

    Create 01_beginning.R

---
+++ 01_beginning.R
@@ -0,0 +1,28 @@
+# First R scriptFirst 
+
+#R as a calculator
+a <- 6*7
+b <- 5*8
+
+a+b
+
+# Arrays
+flowers <-c(3,6,8,10,15,18)
+flowers
+insects<-c(10,16,25,42,61,73)
+insects
+
+plot(flowers,insects)
+
+# changing plot parametres
+
+# symbols
+plot(flowers,insects,pch=19)
+
+#symbol dimension (carachter exageration)
+plot(flowers,insects,pch=19, cex=2)
+plot(flowers,insects,pch=19, cex=0.5)
+
+# symbol color
+plot(flowers,insects,pch=19, cex=2, col="red") this function is cool
+
