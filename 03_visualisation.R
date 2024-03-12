d0019f9af670e8002f33291f480d099c137c90b4
dariopratelli <98481720+dariopratelli@users.noreply.github.com>
2024-03-12 17:42:36 +0100

    Create 03_visualisation.R

---
+++ 03_visualisation.R
@@ -0,0 +1,10 @@
+# Satellite data visualisation in R by imageRy
+
+library(terra)
+library(imageRy)
+
+#list of .......
+im.list()
+
+#importing data
+mato<-im.import("matogrosso_ast_2006209_lrg.jpg")
