25b95e06e128d2bf993647481812f89a3e8c91c6
dariopratelli <98481720+dariopratelli@users.noreply.github.com>
2024-03-12 17:01:56 +0100

    Update 02_installing_packages.R

--- 02_installing_packages.R
+++ 02_installing_packages.R
@@ -1,3 +1,13 @@
 # Install packages
 
+
 install.packages("terra")
+library(terra)
+
+#install.packages("devtools")
+library(devtools)
+
+# install the imageRy package from GitHub
+devtools::install_github("/imageRy)
+
+library(imageRy)
