962620ab760d9e7994429c4cff8e6852e256ea91
dariopratelli <98481720+dariopratelli@users.noreply.github.com>
2024-04-11 17:35:41 +0200

    Update 06_times_series.R

--- 06_times_series.R
+++ 06_times_series.R
@@ -24,7 +24,8 @@ plot(difEN, col=cldif)
 dev.off()
 plot(difEN, col=cldif)
 
-### New example: temperature in Greenland
+### New example: temperature and ice melt in Greenland
+#lavoriamo con un proxy del programma copernicus
 
 g2000 <- im.import("greenland.2000.tif")
 clg <- colorRampPalette(c("black", "blue", "white", "red")) (100)
@@ -40,7 +41,7 @@ par(mfrow=c(1,2))
 plot(g2000, col=clg)
 plot(g2015, col=clg)
 
-# stacking the data
+# stacking the data (stack)
 stackg <- c(g2000, g2005, g2010, g2015)
 plot(stackg, col=clg)
 
@@ -48,6 +49,11 @@ plot(stackg, col=clg)
 difg <- stackg[[1]] - stackg[[4]]
 # difg <- g2000 - g2015
 plot(difg, col=cldif)
+dev.off()
+plot(difg, col=cldif)
+
+clgreen <- colorRampPalette(c("red", "white", "blue")) (100) #cambiare l'ordine dei colori per evidenziare gli aumenti di temperatura in rosso
+ plot(difg, col=clgreen)
 
 # Exercise: make a RGB plot using different years
-im.plotRGB(stackg, r=1, g=2, b=3)
+im.plotRGB(stackg, r=1, g=2, b=4) #se la mappa diventa rosso ho valori più alti nel 2000 , se verde nel 2005, se blu nel 2015, servono immagini della stessa dimensione
