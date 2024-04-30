ab8d3d8e65a2625513817c741167eb98a7b41226
dariopratelli <98481720+dariopratelli@users.noreply.github.com>
2024-04-30 20:27:19 +0200

    Update 08_variability.R

--- 08_variability.R
+++ 08_variability.R
@@ -1,42 +1,54 @@
-# Measuring variability from satellite imagery
+# mesuring spatial variability
 
 library(imageRy)
 library(terra)
-# install.packages("viridis")
+
+# vado a installare i pacchetto viridis: permette di fare visualizzazione comprensibili anche a persone affette da daltonismo 
+install.packages("viridis")
 library(viridis)
 
+
 im.list()
-
 sent <- im.import("sentinel.png")
+im.plotRGB(sent, r=1, g=2, b=3) # suolo nudo è diventato celeste e vegetazione è rossa
 
-im.plotRGB(sent, 1, 2, 3)
+# nir è banda 1 perchè abbiamo visto che riflette la vegetazione
 
-# NIR = band 1
-# red = band 2
-# green = band 3
+#nir = band 1
+#red = band 2
+#green = band 3
 
 im.plotRGB(sent, r=2, g=1, b=3)
 
+# ne vogliamo calcolare la variabilità
+# utilizzando il metodo Moving Window
+
+# associamo a una variabile la banda 1 del nir al fine di utilizzarla per i nostri calcoli
 nir <- sent[[1]]
-cl <- colorRampPalette(c("black", "blue", "green", "yellow"))(4)
+plot(nir)
+
+cl <- colorRampPalette(c("red", "orange", "yellow")) (100)
 plot(nir, col=cl)
 
+# funzione focal() ci permette di estrapolare i valori focali: statistiche all'interno di un gruppo di valori, nel nostro caso la dev_stand
+# usiamo una matrice:definiamo il numero di pixcel della matrice 3X3
+# esprimiamo poi la funzione che ci interessa, in questo caso fun=sd - variabilità con la standard deviation
+# NB: non chiamare la funzione sd per non fare confusione tra variabili e funzioni
 sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd)
 plot(sd3)
 
+# cambiamo la palette di colore della visualizzazione con viridis in modo da utilizzare dei colori visibili anche da persone affette da daltonismo
+# usiamo la palette all'interno del pacchetto che si chiama viridis, 7 di una serie di palette, e 256 riguarda il range di sfumature
+
 viridisc <- colorRampPalette(viridis(7))(256)
 plot(sd3, col=viridisc)
+# evidenzia la più alta variabilità geomorfologica 
 
-# Standard deviation 7x7
+# utilizziamo una finestra con un maggiore numero di pixcel della matrice. la deviazione standard cala rispetto al calcolo con la matrice più piccola.
 sd7 <- focal(nir, matrix(1/49, 7, 7), fun=sd)
 plot(sd7, col=viridisc)
 
-# stack
-stacksd <- c(sd3, sd7)
-plot(stacksd, col=viridisc)
+sdstack <- c(sd3, sd7)
+plot(sdstack, col=viridisc)
+# finestre di calcolo più grande mostrano una similarità nella distrubuzione della deviazione standard relativamente all'immagine. dipende dal numero di pixcel
 
-# Standard deviation 13x13
-sd13 <- focal(nir, matrix(1/169, 13, 13), fun=sd)
-
-stacksd <- c(sd3, sd7, sd13)
-plot(stacksd, col=viridisc)
