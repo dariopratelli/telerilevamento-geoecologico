0c3311ee5e15dd4f7daff47e2d7c3f34c5ff220c
dariopratelli <98481720+dariopratelli@users.noreply.github.com>
2024-06-18 17:11:26 +0200

    Update 09_multivariate_analysis.R

--- 09_multivariate_analysis.R
+++ 09_multivariate_analysis.R
@@ -6,45 +6,22 @@ library(viridis) # for the final plot
 
 im.list()
 
-b2 <- im.import("sentinel.dolomites.b2.tif")  #blue
-b3 <- im.import("sentinel.dolomites.b3.tif")  #green
-b4 <- im.import("sentinel.dolomites.b4.tif")  #red
-b8 <- im.import("sentinel.dolomites.b8.tif")  #nir near infra-red
+b2 <- im.import("sentinel.dolomites.b2.tif")  
+b3 <- im.import("sentinel.dolomites.b3.tif")  
+b4 <- im.import("sentinel.dolomites.b4.tif")  
+b8 <- im.import("sentinel.dolomites.b8.tif")  
 
-sentdo <- c(b2, b3, b4, b8)   #sentdo:sentinel dolomites
+sentdo <- c(b2, b3, b4, b8)
 
 im.plotRGB(sentdo, 4, 3, 2)
-im.plotRGB(sentdo, 3, 4, 2)
 
 pairs(sentdo)
 
-# PCA
-
-# 1. Sample
-# sample <- spatSample(sentdo, 100)
-# sample
-
-# 2. PCA : serve a portare un elemento da n bande a una banda
-# pca <- prcomp(sample)
-
-# variance explained
-# summary(pca)
-
-# 3. PCA map
-# pcmap <- predict(sentdo, pca, index=c(1:4))
-
+# pca
 pcimage <- im.pca(sentdo)
 
-# [1] 1663.53820  511.15684   50.79602   31.13307
+tot <- sum(1314.33400, 551.50879, 39.48238, 27.35780)
+1314.33400 * 100 / tot
 
-tot <- sum(1663.53820,  511.15684,  50.79602,   31.13307)
-1663.53820*100 / tot #1
-511.15684*100 / tot #2
-#...3
-#...4
-
-plot(pcimage,col=viridis(100))
-plot(pcimage,col=plasma(100))
-
-magma <- colorRampPalette(magma(7))(255)
-plot(pcimage, col=magma)
+plot(pcimage, col=viridis(100))
+plot(pcimage, col=plasma(100))
